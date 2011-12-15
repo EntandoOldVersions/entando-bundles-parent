4d8518ab377221badb9241eee45dbe3d8149e3aa
trumbitta <trumbitta@gmail.com>
2011-12-15 22:47:10 +0100

    first commit, nothing done
    will reproduce the first layout from: http://designshack.net/articles/layouts/10-rock-solid-website-layout-examples

---
+++ entando-bundles-pages/entando-page-h5c3-3boxes/src/main/sql/postgresql/port.sql
@@ -0,0 +1,5 @@
+INSERT INTO pagemodels (code, descr, frames, plugincode) VALUES ('entando-page-h5c3-3boxes', '3 Boxes', '<frames>
+	<frame pos="0">
+		<descr>Sample I</descr>
+	</frame>
+</frames>', NULL);
