42a32171b6287fd4d4c3a85791b990a927f5338f
trumbitta <trumbitta@gmail.com>
2011-12-19 13:07:12 +0100

    new showlet: search_form

---
+++ entando-bundles-showlets/entando-showlet-search_form/src/main/sql/mysql/port.sql
@@ -0,0 +1,5 @@
+/*!40000 ALTER TABLE `showletcatalog` DISABLE KEYS */;
+LOCK TABLES `showletcatalog` WRITE;
+INSERT INTO `EntandoPort`.`showletcatalog` VALUES  ('entando-showlet-search_form','<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<properties>\n<property key=\"en\">Search Form</property>\n<property key=\"it\">Search Form</property>\n</properties>',NULL,NULL,NULL,NULL,1,'')
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `showletcatalog` ENABLE KEYS */;
---
+++ entando-bundles-showlets/entando-showlet-search_form/src/main/sql/postgresql/port.sql
@@ -0,0 +1,5 @@
+INSERT INTO showletcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('entando-showlet-search_form', '<?xml version="1.0" encoding="UTF-8"?>
+<properties>
+<property key="en">Search Form</property>
+<property key="it">Search Form</property>
+</properties>', NULL, NULL, NULL, NULL, 1, NULL);
