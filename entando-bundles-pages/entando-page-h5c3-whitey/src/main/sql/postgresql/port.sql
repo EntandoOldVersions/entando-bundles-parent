3ceadc87a331b26d70e1dfd81eed5566d9e0824f
trumbitta <trumbitta@gmail.com>
2011-12-20 18:22:36 +0100

    new page bundle: entando-page-h5c3-whitey
    an attempt to build a light page model on top of the showlets used for the dark one (entando-page-h5c3-3boxes)

---
+++ entando-bundles-pages/entando-page-h5c3-whitey/src/main/sql/postgresql/port.sql
@@ -0,0 +1,58 @@
+INSERT INTO pagemodels (code, descr, frames, plugincode) VALUES ('entando-page-h5c3-whitey', 'Whitey', '<frames>
+	<frame pos="0">
+		<descr>Header - Top I</descr>
+		<defaultShowlet code="entando-showlet-language_choose" />
+	</frame>
+	<frame pos="1">
+		<descr>Header - Top II</descr>
+		<defaultShowlet code="entando-showlet-search_form" />
+	</frame>
+	<frame pos="2">
+		<descr>Navigation - Main</descr>
+		<defaultShowlet code="entando-showlet-navigation_menu" />
+	</frame>
+	<frame pos="3" main="true">
+		<descr>Content I</descr>
+	</frame>
+	<frame pos="4">
+		<descr>Content II</descr>
+	</frame>
+	<frame pos="5">
+		<descr>Content III</descr>
+	</frame>
+	<frame pos="6">
+		<descr>Content IV</descr>
+	</frame>
+	<frame pos="7">
+		<descr>Content V</descr>
+	</frame>
+	<frame pos="8">
+		<descr>Aside I</descr>
+		<defaultShowlet code="entando-showlet-navigation_menu" />
+	</frame>
+	<frame pos="9">
+		<descr>Aside II</descr>
+	</frame>	
+	<frame pos="10">
+		<descr>Aside III</descr>
+	</frame>	
+	<frame pos="11">
+		<descr>Aside IV</descr>
+	</frame>
+	<frame pos="12">
+		<descr>Aside V</descr>
+	</frame>
+	<frame pos="13">
+		<descr>Footer I</descr>
+	</frame>
+	<frame pos="14">
+		<descr>Footer II</descr>
+	</frame>
+	<frame pos="15">
+		<descr>Footer III</descr>
+	</frame>
+	<frame pos="16">
+		<descr>Footer IV</descr>
+	</frame>
+</frames>', NULL);
+
