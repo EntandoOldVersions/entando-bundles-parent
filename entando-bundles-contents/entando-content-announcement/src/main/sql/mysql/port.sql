4fae679cadfef70cc153e908afa653c5d506712f
trumbitta <trumbitta@gmail.com>
2012-06-20 18:10:38 +0200

    add Announcement content bundle

---
+++ entando-bundles-contents/entando-content-announcement/src/main/sql/mysql/port.sql
@@ -0,0 +1,8 @@
+-- keep the 10000 set for the bundles
+-- change the x to something suitable and unused
+-- set the last column to NULL if your Content Model doesn't need any stylesheet at all
+-- You'll want to provide at least one Default Content Model, and one Content Model for lists
+INSERT INTO contentmodels (modelid, contenttype, descr, model, stylesheet) VALUES (1000x, 'ANN', 'Full - Default', 'here the content model velocity snippet', 'contentmodels/ANN.css');
+
+INSERT INTO contentmodels (modelid, contenttype, descr, model, stylesheet) VALUES (100x1, 'ANN', 'Lists - Default', 'here the content model velocity snippet', 'contentmodels/ANN.css');
+
