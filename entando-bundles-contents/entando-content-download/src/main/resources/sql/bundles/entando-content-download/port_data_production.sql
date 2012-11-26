4070823c6abdfb8b4ca29f8835d22cbb5fce756d
trumbitta <trumbitta@gmail.com>
2012-11-26 11:09:17 +0100

    add ↓ for more robust call-to-action on download

--- entando-bundles-contents/entando-content-download/src/main/resources/sql/bundles/entando-content-download/port_data_production.sql
+++ entando-bundles-contents/entando-content-download/src/main/resources/sql/bundles/entando-content-download/port_data_production.sql
@@ -4,5 +4,5 @@ INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_CATEGORIES
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_GET_INFO', 'en', 'View details →');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_GET_INFO', 'it', 'Dettagli →');
 
-INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_DOWNLOAD', 'en', 'Get it now');
-INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_DOWNLOAD', 'it', 'Scarica');
+INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_DOWNLOAD', 'en', 'Get it now ↓');
+INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('DLD_DOWNLOAD', 'it', 'Scarica ↓');
