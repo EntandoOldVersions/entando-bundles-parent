ed074aeea58bafdb6975e4942d8f5c79fba3ef8b
William Ghelfi <w.ghelfi@entando.com>
2013-04-11 18:03:24 +0200

    add page bundle for myportalplus plugin - first iteration

---
+++ entando-bundles-pages/entando-page-myportalplus-basic/src/main/resources/sql/bundles/entando-page-myportalplus-basic/port_data_production.sql
@@ -0,0 +1,49 @@
+INSERT INTO pagemodels (code, descr, frames, plugincode) VALUES ('entando-page-myportalplus-basic', 'My Portal - Basic', '<frames>
+  <frame pos="0" locked="true">
+    <descr>Navbar 1</descr>
+    <defaultShowlet code="entando-showlet-language_choose" />
+  </frame>
+  <frame pos="1" locked="true">
+    <descr>Navbar 2</descr>
+    <defaultShowlet code="entando-showlet-navigation_bar">
+      <properties>
+        <property key="navSpec">code(homepage)</property>
+      </properties>
+    </defaultShowlet>
+  </frame>
+  <frame pos="2" locked="true">
+    <descr>Navbar 3</descr>
+    <defaultShowlet code="entando-showlet-search_form" />
+  </frame>
+  <frame pos="3" locked="true">
+    <descr>Navbar 4</descr>
+    <defaultShowlet code="entando-showlet-login_form" />
+  </frame>
+  <frame pos="4" locked="true">
+    <descr>Toolbar 1</descr>
+  </frame>
+  <frame pos="5" column="1" locked="false"><descr>First Column I</descr><defaultShowlet code="jpmyportalplus_void" /></frame>
+  <frame pos="6" column="1" locked="false"><descr>First Column II</descr><defaultShowlet code="jpmyportalplus_void" /></frame>
+  <frame pos="7" column="1" locked="false"><descr>First Column II</descr><defaultShowlet code="jpmyportalplus_void" /></frame>
+  <frame pos="8" column="2" locked="false"><descr>Second Column I</descr><defaultShowlet code="jpmyportalplus_void" /></frame>
+  <frame pos="9" column="2" locked="false"><descr>Second Column II</descr><defaultShowlet code="jpmyportalplus_void" /></frame>
+  <frame pos="10" column="2" locked="false"><descr>Second Column II</descr><defaultShowlet code="jpmyportalplus_void" /></frame>
+  <frame pos="11" column="3" locked="false"><descr>Third Column I</descr><defaultShowlet code="jpmyportalplus_void" /></frame>
+  <frame pos="12" column="3" locked="false"><descr>Third Column II</descr><defaultShowlet code="jpmyportalplus_void" /></frame>
+  <frame pos="13" column="3" locked="false"><descr>Third Column II</descr><defaultShowlet code="jpmyportalplus_void" /></frame>
+  <frame pos="14" locked="true">
+    <descr>Footer 1</descr>
+  </frame>
+  <frame pos="15" locked="true">
+    <descr>Footer 2</descr>
+  </frame>
+  <frame pos="16" locked="true">
+    <descr>Footer 3</descr>
+  </frame>
+  <frame pos="17" locked="true">
+    <descr>Footer 4</descr>
+  </frame>
+  <frame pos="18" locked="true">
+    <descr>Footer 5</descr>
+  </frame>
+</frames>', NULL);
