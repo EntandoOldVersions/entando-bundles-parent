59836047314295f7ecad04924c83a7f00e356676
eugeniosant <eugenio.santoboni@gmail.com>
2014-03-28 18:03:51 +0100

    add myportal page model configuration

--- entando-bundles-pages/entando-page-myportalplus-basic/src/main/resources/sql/bundles/entando-page-myportalplus-basic/port_data_production.sql
+++ entando-bundles-pages/entando-page-myportalplus-basic/src/main/resources/sql/bundles/entando-page-myportalplus-basic/port_data_production.sql
@@ -47,3 +47,26 @@ INSERT INTO pagemodels (code, descr, frames, plugincode) VALUES ('entando-page-m
     <descr>Footer 5</descr>
   </frame>
 </frames>', 'jpmyportalplus');
+
+
+INSERT INTO jpmyportalplus_modelconfig(code, config) VALUES ('entando-page-myportalplus-basic', '<frames>
+  <frame pos="0" locked="true"></frame>
+  <frame pos="1" locked="true"></frame>
+  <frame pos="2" locked="true"></frame>
+  <frame pos="3" locked="true"></frame>
+  <frame pos="4" locked="true"></frame>
+  <frame pos="5" column="1" locked="false"></frame>
+  <frame pos="6" column="1" locked="false"></frame>
+  <frame pos="7" column="1" locked="false"></frame>
+  <frame pos="8" column="2" locked="false"></frame>
+  <frame pos="9" column="2" locked="false"></frame>
+  <frame pos="10" column="2" locked="false"></frame>
+  <frame pos="11" column="3" locked="false"></frame>
+  <frame pos="12" column="3" locked="false"></frame>
+  <frame pos="13" column="3" locked="false"></frame>
+  <frame pos="14" locked="true"></frame>
+  <frame pos="15" locked="true"></frame>
+  <frame pos="16" locked="true"></frame>
+  <frame pos="17" locked="true"></frame>
+  <frame pos="18" locked="true"></frame>
+</frames>');
