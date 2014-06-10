bc986d2a71b33834b8302462b24a34e65e8ad887
Matteo E. Minnai <m.minnai@entando.com>
2014-06-10 19:11:28 +0200

    GUI fragments insertion

--- entando-bundles-widgets/entando-widget-language_choose/src/main/resources/sql/bundles/entando-widget-language_choose/port_data_production.sql
+++ entando-bundles-widgets/entando-widget-language_choose/src/main/resources/sql/bundles/entando-widget-language_choose/port_data_production.sql
@@ -4,6 +4,34 @@ INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode,
 <property key="it">Choose a Language</property>
 </properties>', NULL, NULL, NULL, NULL, 1, NULL);
 
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('entando-widget-language_choose', 'entando-widget-language_choose', NULL, NULL, '<#assign c=JspTaglibs["http://java.sun.com/jsp/jstl/core"]>
+<#assign wp=JspTaglibs["/aps-core"]>
+
+<@wp.headInfo type="JS" info="entando-misc-jquery/jquery-1.10.0.min.js" />
+<@wp.headInfo type="JS" info="entando-misc-bootstrap/bootstrap.min.js" />
+<@wp.info key="langs" var="langsVar" />
+<@wp.info key="currentLang" var="currentLangVar" />
+
+<ul class="nav">
+  <li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="icon-flag"></span> <@wp.i18n key="ESLC_LANGUAGE" /> <span class="caret"></span></a>
+      <ul class="dropdown-menu">
+				<@wp.freemarkerTemplateParameter var="langsListVar" valueName="langsVar" removeOnEndTag=true >
+				
+				<#list langsListVar as curLangVar>
+				<li
+							<#if (curLangVar.code == currentLangVar)>class="active" </#if>>
+							<a href=<@wp.url lang="${curLangVar.code}" paramRepeat=true />">
+								<@wp.i18n key="ESLC_LANG_${curLangVar.code}" />
+							</a>
+				</li>
+				</#list>
+				
+				</@wp.freemarkerTemplateParameter>
+      </ul>
+  </li>
+</ul>', 1);
+
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLC_LANGUAGE', 'en', 'Language');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLC_LANGUAGE', 'it', 'Lingua');
 
--- entando-bundles-widgets/entando-widget-login_form/src/main/resources/sql/bundles/entando-widget-login_form/port_data_production.sql
+++ entando-bundles-widgets/entando-widget-login_form/src/main/resources/sql/bundles/entando-widget-login_form/port_data_production.sql
@@ -4,6 +4,72 @@ INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode,
 <property key="it">Dropdown Sign In</property>
 </properties>', NULL, NULL, NULL, NULL, 1, NULL);
 
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('entando-widget-login_form', 'entando-widget-login_form', NULL, NULL, '<#assign wp=JspTaglibs["/aps-core"]>
+<#assign c=JspTaglibs["http://java.sun.com/jsp/jstl/core"]>
+<@wp.headInfo type="JS" info="entando-misc-jquery/jquery-1.10.0.min.js" />
+<@wp.headInfo type="JS" info="entando-misc-bootstrap/bootstrap.min.js" />
+
+<ul class="nav pull-right">
+	<li class="span2 dropdown
+<#if (accountExpired?? && accountExpired == true) || (wrongAccountCredential?? && wrongAccountCredential == true)>open</#if>
+">
+
+	<#if (Session.currentUser != "guest")>
+			<div class="btn-group">
+				<button class="btn span2 text-left dropdown-toggle" data-toggle="dropdown">
+					<@c.out value="${Session.currentUser}" />
+					<span class="caret pull-right"></span>
+				</button>
+				<ul class="dropdown-menu pull-right well-small">
+					<li class="padding-medium-vertical">
+
+						<@wp.ifauthorized permission="enterBackend">
+						<p>
+							<a href="<@wp.info key="systemParam" paramName="applicationBaseURL" />do/main.action?request_locale=<@wp.info key="currentLang" />"><span class="icon-wrench"></span> <@wp.i18n key="ESLF_ADMINISTRATION" /></a>
+						</p>
+						</@wp.ifauthorized>
+						<div class="divider"></div>
+						<p class="help-block text-right">
+							<a class="btn" href="<@wp.info key="systemParam" paramName="applicationBaseURL" />do/logout.action"><@wp.i18n key="ESLF_SIGNOUT" /></a>
+						</p>
+						<@wp.pageWithWidget var="editProfilePageVar" widgetTypeCode="userprofile_editCurrentUser" />
+						<#if (editProfilePageVar??) >
+						<p class="help-block text-right">
+							<a href="<@wp.url page="${editProfilePageVar.code}" />" ><@wp.i18n key="ESLF_PROFILE_CONFIGURATION" /></a>
+						</p>
+						</#if>
+					</li>
+				</ul>
+			</div>	
+		<#else>
+			<a class="dropdown-toggle text-right" data-toggle="dropdown" href="#"><@wp.i18n key="ESLF_SIGNIN" /> <span class="caret"></span></a>
+			<ul class="dropdown-menu well-small">
+				<li>
+					<form class="form-vertical" method="POST">
+						<#if (accountExpired?? && accountExpired == true)>
+						<div class="alert alert-error">
+							<button class="close" data-dismiss="alert">x</button>
+							<@wp.i18n key="ESLF_USER_STATUS_EXPIRED" />
+						</div>
+						</#if>
+						<#if (wrongAccountCredential?? && wrongAccountCredential == true)>
+						<div class="alert alert-error">
+							<button class="close" data-dismiss="alert">x</button>
+							<@wp.i18n key="ESLF_USER_STATUS_CREDENTIALS_INVALID" />
+						</div>
+						</#if>
+						<input type="text" name="username" class="input-large" placeholder="<@wp.i18n key="ESLF_USERNAME" />">
+						<input type="password" name="password" class="input-large" placeholder="<@wp.i18n key="ESLF_PASSWORD" />">
+						<p class="text-right">
+							<input type="submit" class="btn btn-primary" value="<@wp.i18n key="ESLF_SIGNIN" />" />
+						</p>
+					</form>
+				</li>
+			</ul>
+		</#if>
+	</li>
+</ul>', 1);
+
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_WELCOME', 'en', 'Hello');
 INSERT INTO localstrings(keycode, langcode, stringvalue) VALUES ('ESLF_WELCOME', 'it', 'Ciao');
 
